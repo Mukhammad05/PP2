a61d7ec9b753e839b6177bc7d672fff4283b64bb
MukhammadAli05 <fespenbetova@mail.ru>
2024-02-03 16:31:12 +0300

    my first commit

---
+++ Lab2/Join Tuples.py
@@ -0,0 +1,28 @@
+# task 1
+tuple1 = ("a", "b" , "c")
+tuple2 = (1, 2, 3)
+
+tuple3 = tuple1 + tuple2
+print(tuple3)
+
+# task 2
+fruits = ("apple", "banana", "cherry")
+mytuple = fruits * 2
+
+print(mytuple)
+
+# task 3
+fruits = ("apple", "banana", "cherry")
+print(fruits[0])
+
+# task 4
+fruits = ("apple", "banana", "cherry")
+print(len(fruits))
+
+# task 5
+fruits = ("apple", "banana", "cherry") 
+print(fruits[-1])
+
+# task 6
+fruits = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(fruits[2:5])
