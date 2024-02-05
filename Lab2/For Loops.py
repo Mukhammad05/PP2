2e34c524370aec41ae01e43d247ae644ccb12cf2
MukhammadAli05 <fespenbetova@mail.ru>
2024-02-05 16:31:54 +0300

    my first commit

---
+++ Lab2/For Loops.py
@@ -0,0 +1,90 @@
+# task 1
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x) 
+
+# task 2
+for x in "banana":
+  print(x) 
+
+# task 3
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x) 
+  if x == "banana":
+    break
+
+# task 4
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x) 
+
+# task 5
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x) 
+
+# task 6
+for x in range(6):
+  print(x) 
+
+# task 7
+for x in range(2, 6):
+  print(x) 
+
+# task 8
+for x in range(2, 30, 3):
+  print(x) 
+
+# task 9
+for x in range(6):
+  print(x)
+else:
+  print("Finally finished!")
+
+# task 10
+for x in range(6):
+  if x == 3: break
+  print(x)
+else:
+  print("Finally finished!")
+
+# task 11
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
+
+# task 12
+for x in [0, 1, 2]:
+  pass
+
+# task 13
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+# task 14
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+print(x)
+
+# task 15
+for x in range(6):
+  print(x)
+
+# task 16
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+
+  print(x)
